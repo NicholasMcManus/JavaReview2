293422bfc48049a71fa06d20b35059d8715dcfba
NicholasMcManus <mcmanusnj3@gmail.com>
2019-09-24 11:38:24 -0400

    First commit, Files to come

---
+++ src/polyandinherit/PolyAndInherit.java
@@ -0,0 +1,21 @@
+/*
+ * file name: PolyAndInherit.java
+ * programmer name: Nick McManus
+ * date created: 09-24-2019
+ * date of last revision: 
+ * details of last revision:
+ * short description: 
+ */
+
+package polyandinherit;
+
+public class PolyAndInherit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
