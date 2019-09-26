9e746d15383c3f671f30d033400869cd8d488513
NicholasMcManus <mcmanusnj3@gmail.com>
2019-09-26 14:24:17 -0400

    Finish up Employee

--- src/polyandinherit/Employee.java
+++ src/polyandinherit/Employee.java
@@ -9,10 +9,45 @@
 
 package polyandinherit;
 
-public class Employee {
+public class Employee extends Person{
     private int office;
     private double salary;
     private MyDate dateHired;
+    
+    public Employee()
+    {
+        super();
+        this.office = 0;
+        this.salary = 0.0;
+        this.dateHired = new MyDate();
+    }
+
+    public Employee(int office, double salary, MyDate dateHired, String name, String address, String email, int phoneNumber) {
+        super(name, address, email, phoneNumber);
+        this.office = office;
+        this.salary = salary;
+        this.dateHired = dateHired;
+    }
+
+    public int getOffice() {
+        return office;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public MyDate getDateHired() {
+        return dateHired;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee: " + this.getName(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
     
     
 }
