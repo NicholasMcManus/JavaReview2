f1bd431f37acae02c8c316f5edbaa0a8c6544911
NicholasMcManus <mcmanusnj3@gmail.com>
2019-09-30 12:33:14 -0400

    Finish writing Classes

--- src/polyandinherit/Staff.java
+++ src/polyandinherit/Staff.java
@@ -3,12 +3,41 @@
  * programmer name: Nick McManus
  * date created: 09-26-2019
  * date of last revision: 
- * details of last revision:
- * short description: 
+ * details of last revision: Write the staff class
+ * short description: Describe a class, which is the child of an employee
  */
 
 package polyandinherit;
 
-public class Staff {
+public class Staff extends Employee{
+    //Class variables
+    private String title;
+    
+    //Constructors
+    //No Argument Constructor
+    public Staff() {
+        super();
+        title = "Intern";
+    }
 
+    //Full Argument Construtor
+    public Staff(String title, int office, double salary, MyDate dateHired, String name, String address, String email, int phoneNumber) {
+        super(office, salary, dateHired, name, address, email, phoneNumber);
+        this.title = title;
+    }
+    
+    //Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    //Override toString
+    @Override
+    public String toString() {
+        return "Staff: " + this.getName();
+    }
 }
